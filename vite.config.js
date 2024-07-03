1847723e4d590ee058b8c973d057d2ce0d88d1e1
Andygggg <andyhello31468@gmail.com>
2024-07-03 16:11:49 +0800

    fix: 2

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,49 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, '')
-  return {
-    // vite config設置
-    plugins: [vue()],
-    publicDir: 'public',
-    base: './',
-    server: {
-      open: true,
-      https: false,
-      host: env.VITE_APP_HOST,
-      port: parseInt(env.VITE_APP_PORT),
-      proxy: null,
-      cors: true,
+// export default defineConfig(({ mode }) => {
+//   // Load env file based on `mode` in the current working directory.
+//   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+//   const env = loadEnv(mode, '')
+//   return {
+//     // vite config設置
+//     plugins: [vue()],
+//     publicDir: 'public',
+//     base: '/github_pages/',
+//     server: {
+//       open: true,
+//       https: false,
+//       host: env.VITE_APP_HOST,
+//       port: parseInt(env.VITE_APP_PORT),
+//       proxy: null,
+//       cors: true,
+//     },
+//     preview: {
+//       open: true,
+//       https: false,
+//       host: env.VITE_APP_HOST,
+//       port: parseInt(env.VITE_APP_PORT),
+//       proxy: null,
+//       cors: true,
+//     },
+//     build: {
+//       outDir: 'dist',
+//     },
+//     resolve: {
+//       alias: {
+//         '@': fileURLToPath(new URL('./src', import.meta.url)),
+//       },
+//     },
+//   }
+// })
+export default defineConfig({
+  plugins: [vue()],
+  base: '/github_pages/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    preview: {
-      open: true,
-      https: false,
-      // host: env.VITE_APP_HOST,
-      // port: parseInt(env.VITE_APP_PORT),
-      proxy: null,
-      cors: true,
-    },
-    build: {
-      outDir: 'dist',
-    },
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
-    },
-  }
+  },
 })
