afd983c7258ed80c91c3f5dc4f8f425fd2caaa7d
Andygggg <andyhello31468@gmail.com>
2024-07-03 16:35:56 +0800

    4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  123
+  <RouterView />
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
 </script>
 
-<!-- <style lang="scss">
+<style lang="scss">
 @import './assets/all';
-</style> -->
+</style>
